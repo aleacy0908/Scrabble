cf0a9b11a3c2d4448d04b26c5acc2f25bf5a4344
ryanj00 <48124562+ryanj00@users.noreply.github.com>
2020-02-10 11:42:03 +0000

    All Tests implemented

--- PlayerTest.java
+++ PlayerTest.java
@@ -10,15 +10,19 @@ import org.junit.jupiter.api.Test;
 
 class PlayerTest {
 
+    int score = 34;
+
     @Test
     public void TestgetName ( ) {
 
 
-        Player j = new Player("bill", "john", 56, 67);
+        Player j = new Player("bill", 56);
 
 
-        assert (j.nameP1().equals("bill"));
-        assert (j.nameP2().equals("john"));
+        assert (j.nameP().equals("bill"));
+        assertFalse(j.nameP().equals("john"));
+
+        System.out.println("getName Test Succesful");
     }
 
     @Test
@@ -27,13 +31,14 @@ class PlayerTest {
         String name = "john";
         String name2 = "bill";
 
-        Player j = new Player("john", "bill", 56, 67);
+        Player j = new Player("john",67);
 
-        j.setPlayer1(name);
-        j.setPlayer2(name2);
+        j.setPlayer(name);
 
-        assert(j.nameP1().equals("john"));
-        assert(j.nameP2().equals("bill"));
+        assert(j.nameP().equals("john"));
+
+        System.out.println("setName Test Succesful");
+
 
 
     }
@@ -42,31 +47,30 @@ class PlayerTest {
     public void Testreset( ) {
 
         //String blank = "";
-        Player j = new Player("john", "bill", 56, 67);
+        Player j = new Player("john", 67);
 
-        assert(j.nameP1().equals("john"));
-        assert(j.nameP2().equals("bill"));
+        assert(j.nameP().equals("john"));
+
 
 
         j.reset();
 
-        assert(j.nameP1().equals(""));
-        assert(j.nameP2().equals(""));
+        assert(j.nameP().equals(""));
+
+        System.out.println("reset Test Succesful");
 
     }
 
     @Test
     public void TestgetScore( ) {
 
-        Player j = new Player("john", "bill", 57, 68);
+        Player j = new Player("john",68);
 
-       j.getP1_score();
+        j.getP_score();
 
-        assertEquals(57, j.getP1_score());
+        assertEquals(68, j.getP_score());
 
-        j.getP2_score();
-
-        assertEquals(68, j.getP2_score());
+        System.out.println("getScore Test Succesful");
 
     }
 
@@ -77,14 +81,38 @@ class PlayerTest {
         Frame frameP1 = new Frame();
         Frame frameP2 = new Frame();
 
-        frameP1.frame = new ArrayList<Character>(Arrays.asList('E', 'C', 'X', 'S', 'K'));
-        frameP2.frame = new ArrayList<Character>(Arrays.asList('S', 'W', 'Q', 'A', 'B'));
+        frameP1.frame = new ArrayList<Character>(Arrays.asList('E', 'C', 'X', 'S', 'K','J'));
+        frameP2.frame = new ArrayList<Character>(Arrays.asList('S', 'W', 'Q', 'A', 'B','L'));
 
-        assertEquals(Arrays.asList('E', 'C', 'X', 'S', 'K'),frameP1.getFrame());
-        assertEquals(Arrays.asList('S', 'W', 'Q', 'A', 'B'),frameP2.getFrame());
+        assertEquals(Arrays.asList('E', 'C', 'X', 'S', 'K','J'),frameP1.getFrame());
+        assertEquals(Arrays.asList('S', 'W', 'Q', 'A', 'B','L'),frameP2.getFrame());
+
+        System.out.println("getFrame Test Succesful");
 
 
     }
 
+    @Test
+    public void TestincreaseScore() {
+
+
+        Player j = new Player("john",57);
+
+
+        j.getP_score();
+
+        assertEquals(57, j.getP_score());
+
+        j.increaseScore(10);
+
+
+        assertEquals(67, j.getP_score());
+
+        System.out.println("getScore Test Successful");
 
     }
+
+
+}
+
+
