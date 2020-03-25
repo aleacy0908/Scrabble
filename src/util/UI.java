813537e01bfe2fcd253eba49661e069c956090f0
ryanj00 <48124562+ryanj00@users.noreply.github.com>
2020-03-25 18:41:53 +0000

    changes

--- src/util/UI.java
+++ src/util/UI.java
@@ -22,44 +22,85 @@ import javafx.scene.Node;
 
 public class UI extends Application {
 
-    public final short  B_ROWS = 15;
-    public final short  B_COLS = 15;
+    public final short B_ROWS = 15;
+    public final short B_COLS = 15;
     Board board;
 
-    public Parent createBoard(Board b)
-    {
+    int i, j;
+
+    public class Tile extends StackPane {
+        public Tile (String a) {
+
+            Rectangle border = new Rectangle(50, 50);
+            border.setFill(Color.GREEN);
+            border.setStroke(Color.WHITE);
+
+            GridPane.setRowIndex(border, i);
+            GridPane.setColumnIndex(border, j);
+
+
+            Text text = new Text(a);
+            // text.setFill(Color.BLACK);
+            setAlignment(Pos.CENTER);
+            getChildren().addAll(border, text);
+
+        }
+    }
+
+
+    public Parent createBoard (Board b) {
         this.board = b;
 
-        GridPane gameBoard = new GridPane();
+        Pane gameBoard = new Pane();
         gameBoard.setPrefSize(700, 700);
 
-        for(int i = 1; i < 16; i++)
-        {
-            for(int j = 1; j < 16; j++)
-            {
-                Rectangle border = new Rectangle(50, 50);
-                border.setFill(Color.GREEN);
-                border.setStroke(Color.WHITE);
+        for (i = 1; i < 16; i++) {
+            for (j = 1; j < 16; j++) {
+                Square sqr = board.getSquare(i, j);
 
-                GridPane.setRowIndex(border, i);
-                GridPane.setColumnIndex(border, j);
+                if (sqr.isOccupied()) {
 
-                gameBoard.getChildren().addAll(border);
+                    char tile = sqr.getTile();
+                    Tile bt = new Tile(String.valueOf(tile));
+
+                } else {
+                    String output;
+
+                    switch (sqr.getMultiplier()) {
+                        case DL:
+                            output = "DL";
+                            Tile bt = new Tile(String.valueOf(output));
+                            break;
+                        case DW:
+                            output = "DW";
+                            bt = new Tile(String.valueOf(output));
+                            break;
+                        case TL:
+                            output = "TL";
+                            bt = new Tile(String.valueOf(output));
+                            break;
+                        case TW:
+                            output = "TW";
+                            bt = new Tile(String.valueOf(output));
+                            break;
+                        default:
+                            output = "  ";
+                            bt = new Tile(String.valueOf(output));
+
+                            gameBoard.getChildren().add(bt);
+                    }
+                }
             }
         }
-
         return gameBoard;
     }
 
 
-
-    public Board getBoard()
-    {
+    public Board getBoard ( ) {
         return this.board;
     }
 
-    public void setBoard(Board b)
-    {
+    public void setBoard (Board b) {
         this.board = b;
     }
 
@@ -67,14 +108,16 @@ public class UI extends Application {
     @Override
     public void start (Stage primaryStage) {
 
-        primaryStage.setScene(new Scene(createBoard(this.board)));
+        primaryStage.setScene(new Scene(createBoard(new Board())));
         primaryStage.show();
 
 
     }
-
-
-   /* public static void main (String[] args) {
-        launch(args);
-    }*/
 }
+
+/*
+    public static void main (String[] args) {
+        launch(args);
+    }
+}
+*/
