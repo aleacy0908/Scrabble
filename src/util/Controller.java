2ce90c009ee8daea57a5eb52c922126ce7f66453
Adam Leacy <48072343+aleacy0908@users.noreply.github.com>
2020-03-25 02:51:48 +0000

    Changed UI layout & added textEntered method

--- src/util/Controller.java
+++ src/util/Controller.java
@@ -1,20 +1,29 @@
 package src.util;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Controller {
 
+    private static Stage window;
+    public TextField input;
+    public TextArea output;
+
     public static void alert(String title, String message){
-        Stage window = new Stage();
+        window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMaxWidth(350);
+        window.setMinWidth(250);
         Label label = new Label();
         label.setText(message);
 
@@ -31,19 +40,21 @@ public class Controller {
     }
 
     static void closeGame(){
-        boolean answer = confirmBox("Title","Are you sure you want to end the game?");
+        boolean answer = confirm("Exit Game","Are you sure you want to end the game?");
         if(answer){
-
+            Platform.exit();
         }
     }
 
     static boolean answer;
 
-    public static boolean confirmBox(String title, String message){
-        Stage window = new Stage();
+    public static boolean confirm(String title, String message){
+        window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
         window.setMaxWidth(350);
+        window.setMinWidth(250);
+
         Label label = new Label();
         label.setText(message);
 
@@ -60,9 +71,10 @@ public class Controller {
             window.close();
         });
 
-        VBox layout = new VBox(10);
+        VBox layout = new VBox(5);
         layout.getChildren().addAll(label, yes, no);
         layout.setAlignment(Pos.CENTER);
+
         Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait();
@@ -70,4 +82,24 @@ public class Controller {
         return answer;
     }
 
+    public void textEntered(ActionEvent actionEvent) {
+        String option = input.getText();
+        option = option.toUpperCase();
+
+        if(option.equals("QUIT")){
+            closeGame();
+        }
+
+        if(option.equals("PASS")){
+        }
+
+        if(option.equals("HELP")){
+            output.setText(output.getText() + "\n" +"How to use:\n" +
+                    "<GRID REF> <DIRECTION A(across)/D(downwards)> <WORD>\n(Example: A1 A HELLO)\n" +
+                    "QUIT: Close the game\n" +
+                    "HELP: Displays this message");
+        }
+
+
+    }
 }
